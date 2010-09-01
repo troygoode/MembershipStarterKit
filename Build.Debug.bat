7fb06c9092ffb52aad23f74f3f4d217b98595a83
Troy Goode <troygoode@gmail.com>
2010-09-01 09:43:59 -0400

    Upgaded Build.Debug.bat and Build.Release.bat to use .NET 4.0 version of MSBuild (3.5 version won't recognized VS2010 sln/csproj files quite right).

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SYSTEMROOT%\Microsoft.Net\Framework\v3.5\msbuild.exe "MvcMembership.sln" /t:rebuild /p:Configuration=Debug
+%SYSTEMROOT%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe "MvcMembership.sln" /t:rebuild /p:Configuration=Debug
 PAUSE
--- Build.Release.bat
+++ Build.Release.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-%SYSTEMROOT%\Microsoft.Net\Framework\v3.5\msbuild.exe "MvcMembership.sln" /t:rebuild /p:Configuration=Release
+%SYSTEMROOT%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe "MvcMembership.sln" /t:rebuild /p:Configuration=Release
 PAUSE
