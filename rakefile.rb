8783d810073be515d78247ce19892f019217e655
Troy Goode <troygoode@gmail.com>
2011-07-14 00:00:43 -0400

    finished changes to rakefile necessary to generate nuget packages

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,5 @@
 require 'albacore'
+require 'find'
 
 task :default => [:build]
 
@@ -51,12 +52,12 @@ task :prepare_package_mvcmembershipmvc => :release do
   controllers_directory = area_directory + 'Controllers/'
   models_directory = area_directory + 'Models/UserAdministration/'
   views_directory = area_directory + 'Views/'
-  views_useradministration_directory = views_directory + 'UserAdministration'
+  views_useradministration_directory = views_directory + 'UserAdministration/'
 
   output_root = './packages/MvcMembership.Mvc/content/'
   output_appstart_directory = output_root + 'App_Start/'
   output_content_directory = output_root + 'Content/'
-  output_area_directory = output_root + 'Areas/MvcMembership'
+  output_area_directory = output_root + 'Areas/MvcMembership/'
   output_controllers_directory = output_area_directory + 'Controllers/'
   output_models_directory = output_area_directory + 'Models/UserAdministration/'
   output_views_directory = output_area_directory + 'Views/'
@@ -70,21 +71,34 @@ task :prepare_package_mvcmembershipmvc => :release do
   FileUtils.mkdir_p output_views_directory
   FileUtils.mkdir_p output_views_useradministration_directory
 
-  FileUtils.cp appstart_directory + 'MvcMembership.cs', output_appstart_directory + 'MvcMembership.cs'
+  FileUtils.cp appstart_directory + 'MvcMembership.cs', output_appstart_directory + 'MvcMembership.cs.pp'
   FileUtils.cp content_directory + 'MvcMembership.css', output_content_directory + 'MvcMembership.css'
-  FileUtils.cp area_directory + 'MvcMembershipAreaRegistration.cs', output_area_directory + 'MvcMembershipAreaRegistration.cs'
-  FileUtils.cp controllers_directory + 'UserAdministrationController.cs', output_controllers_directory + 'UserAdministrationController.cs'
-  FileUtils.cp models_directory + 'CreateUserViewModel.cs', output_models_directory + 'CreateUserViewModel.cs'
-  FileUtils.cp models_directory + 'DetailsViewModel.cs', output_models_directory + 'DetailsViewModel.cs'
-  FileUtils.cp models_directory + 'IndexViewModel.cs', output_models_directory + 'IndexViewModel.cs'
-  FileUtils.cp models_directory + 'RoleViewModel.cs', output_models_directory + 'RoleViewModel.cs'
+  FileUtils.cp area_directory + 'MvcMembershipAreaRegistration.cs', output_area_directory + 'MvcMembershipAreaRegistration.cs.pp'
+  FileUtils.cp controllers_directory + 'UserAdministrationController.cs', output_controllers_directory + 'UserAdministrationController.cs.pp'
+  FileUtils.cp models_directory + 'CreateUserViewModel.cs', output_models_directory + 'CreateUserViewModel.cs.pp'
+  FileUtils.cp models_directory + 'DetailsViewModel.cs', output_models_directory + 'DetailsViewModel.cs.pp'
+  FileUtils.cp models_directory + 'IndexViewModel.cs', output_models_directory + 'IndexViewModel.cs.pp'
+  FileUtils.cp models_directory + 'RoleViewModel.cs', output_models_directory + 'RoleViewModel.cs.pp'
   FileUtils.cp views_directory + 'Web.config', output_views_directory + 'Web.config'
-  FileUtils.cp views_useradministration_directory + 'CreateUser.cshtml', output_views_useradministration_directory + 'CreateUser.cshtml'
-  FileUtils.cp views_useradministration_directory + 'Details.cshtml', output_views_useradministration_directory + 'Details.cshtml'
-  FileUtils.cp views_useradministration_directory + 'Index.cshtml', output_views_useradministration_directory + 'Index.cshtml'
-  FileUtils.cp views_useradministration_directory + 'Password.cshtml', output_views_useradministration_directory + 'Password.cshtml'
-  FileUtils.cp views_useradministration_directory + 'Role.cshtml', output_views_useradministration_directory + 'Role.cshtml'
-  FileUtils.cp views_useradministration_directory + 'UsersRoles.cshtml', output_views_useradministration_directory + 'UsersRoles.cshtml'
+  FileUtils.cp views_useradministration_directory + 'CreateUser.cshtml', output_views_useradministration_directory + 'CreateUser.cshtml.pp'
+  FileUtils.cp views_useradministration_directory + 'Details.cshtml', output_views_useradministration_directory + 'Details.cshtml.pp'
+  FileUtils.cp views_useradministration_directory + 'Index.cshtml', output_views_useradministration_directory + 'Index.cshtml.pp'
+  FileUtils.cp views_useradministration_directory + 'Password.cshtml', output_views_useradministration_directory + 'Password.cshtml.pp'
+  FileUtils.cp views_useradministration_directory + 'Role.cshtml', output_views_useradministration_directory + 'Role.cshtml.pp'
+  FileUtils.cp views_useradministration_directory + 'UsersRoles.cshtml', output_views_useradministration_directory + 'UsersRoles.cshtml.pp'
+end
+
+task :replace_package_contents_mvcmembershipmvc => :prepare_package_mvcmembershipmvc do
+  output_root = 'packages/MvcMembership.Mvc/content/'
+  Find.find output_root do |path|
+  	#puts path
+  	if File.file? path
+     text = File.read path
+     File.open path, 'w' do |file|
+       file.puts text.gsub /SampleWebsite\.Mvc3/, '$rootnamespace$'
+     end
+    end
+  end
 end
 
 exec :package_mvcmembership => :prepare_package_mvcmembership do |cmd|
@@ -97,7 +111,7 @@ exec :package_mvcmembership => :prepare_package_mvcmembership do |cmd|
 	]
 end
 
-exec :package_mvcmembershipmvc => :prepare_package_mvcmembershipmvc do |cmd|
+exec :package_mvcmembershipmvc => :replace_package_contents_mvcmembershipmvc do |cmd|
 	cmd.path_to_command = 'nuget'
 	cmd.parameters [
 		'pack',
