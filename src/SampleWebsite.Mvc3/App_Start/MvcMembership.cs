7ff9deb199b93ddc69e2b54c386760bfa5ace622
Troy Goode <troygoode@gmail.com>
2011-07-13 23:06:20 -0400

    renamed App_Start file to MvcMembership to make it more clear where it comes from

---
+++ src/SampleWebsite.Mvc3/App_Start/MvcMembership.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using MvcMembership;
+using WebActivator;
+
+[assembly: PreApplicationStartMethod(typeof (SampleWebsite.Mvc3.App_Start.MvcMembership), "Start")]
+
+namespace SampleWebsite.Mvc3.App_Start
+{
+	public static class MvcMembership
+	{
+		public static void Start()
+		{
+			GlobalFilters.Filters.Add(new TouchUserOnEachVisitFilter(() => new AspNetMembershipProviderWrapper()));
+		}
+	}
+}
